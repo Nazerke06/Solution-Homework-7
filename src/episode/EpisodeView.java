cf35d8be9d888c562af9732645b3df130f0dda41
Nazerke06 <abdumaliknazerke9@gmail.com>
2025-04-23 20:16:00 +0500

    Fourth commmit

--- src/episode/EpisodeView.java
+++ src/episode/EpisodeView.java
@@ -1,4 +1,20 @@
 package episode;
 
 public class EpisodeView {
+    private Episode episode;
+    private int startOffset;
+
+    public EpisodeView(Episode episode, int startOffset) {
+        this.episode = episode;
+        this.startOffset = startOffset;
+    }
+
+    public void play() {
+        System.out.println("Playing '" + episode.getTitle() + "' starting at " + startOffset + " seconds");
+    }
+
+    public Episode getEpisode() {
+        return episode;
+    }
 }
+
