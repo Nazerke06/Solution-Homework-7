01413a4dd70882d8be690747d615131105c7b7c2
Nazerke06 <abdumaliknazerke9@gmail.com>
2025-04-24 00:16:38 +0500

    Nineth commit

---
+++ src/iterator/ShuffleSeasonIterator.java
@@ -0,0 +1,26 @@
+package iterator;
+
+import episode.Episode;
+
+import java.util.*;
+
+public class ShuffleSeasonIterator implements EpisodeIterator {
+    private List<Episode> episodes;
+    private int currentIndex = 0;
+
+    public ShuffleSeasonIterator(List<Episode> episodes) {
+        this.episodes = new ArrayList<>(episodes);
+        Collections.shuffle(this.episodes, new Random(42));
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < episodes.size();
+    }
+
+    @Override
+    public Episode next() {
+        if (!hasNext()) throw new NoSuchElementException();
+        return episodes.get(currentIndex++);
+    }
+}
